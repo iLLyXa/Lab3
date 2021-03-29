0ce681bec1d0e8441c7b53ac9a386684808aabfe
wseiuser <maillya159753@gmail.com>
2021-03-29 14:13:37 +0200

    Version 2

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,18 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
 public class Controller {
+    public Button btnMain;
+    public TextField txtLicznik;
+    int clickCounter = 0;
+
+    public void onBtnAction(ActionEvent actionEvent) {
+        txtLicznik.setText("Kliknięcie nr " + ++clickCounter);
+        if (clickCounter ==  1)
+            btnMain.setText("I jeszcze raz");
+    }
 }
